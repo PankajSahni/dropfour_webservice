18d2e780c715615115f36199644ea3cc9c2cac00
Akhlaq Ahmad <amu.akhlaq@gmail.com>
2012-10-10 12:26:25 +0530

    updated user.php file

--- user.php
+++ user.php
@@ -1,26 +1,45 @@
 <?php
+include("includes/config.php");
+$response = array();
 
-require_once 'connection.php';
-$array_response = array();
-$current_date = date("Y-m-d H:i:s");
-$json_data = stripslashes($_REQUEST['json_data']);
-$json_array = json_decode($json_data,TRUE);
-$fb_id = $json_array['fb_profileid'];
-$name = $json_array['name'];
-$query_check_if_fb_id_exist = mysql_query("SELECT * FROM user WHERE fb_id = $fb_id");
-$array_response["status"] = "0";
-if(mysql_num_rows($query_check_if_fb_id_exist) == 0)
-{
-    mysql_query("INSERT INTO user SET fb_id = $fb_id,
-    name = '$name', invited_by = 0, created_at = '$current_date', updated_at = '$current_date'");
-    $array_response["status"] = "1";
-    $array_response["action"] = "record inserted";
+if(isset($_POST) && count($_POST) || 1){
+	$current_date = date("Y-m-d H:i:s");
+
+	$json_data = stripslashes($_REQUEST['json_data']);
+
+	$json_array = json_decode($json_data,TRUE);
+
+	if(isset($json_array) && count($json_array)){
+		@extract($json_array);
+		$res_select = mysql_query("select * from user where fb_id='$fb_id'") or die(mysql_error());
+		if(!mysql_num_rows($res_select)){
+			$sql_insert = "insert into user set fb_id='$fb_id',
+				name='$name',
+				status=1,
+				invited_by=0,
+				created_at='$current_date',
+				updated_at='$current_date'";
+			mysql_query($sql_insert) or die(mysql_error());
+			$response['STATUS'] = "1";
+			$response['MESSAGE'] = "Data inserted successfully!";
+		}else{
+			$sql_update = "update user set
+				name='$name',
+				status=1,
+				updated_at='$current_date' where fb_id='$fb_id'";
+			mysql_query($sql_update) or die(mysql_error());
+			$response['STATUS'] = "1";
+			$response['MESSAGE'] = "Data updated successfully!";
+		}
+
+	}else{
+		$response['STATUS'] = "0";
+		$response['MESSAGE'] = "No data found in json data";
+	}
+
+}else{
+	$response['STATUS'] = "0";
+	$response['MESSAGE'] = "You are not using POST method to send data";
 }
-else
-{
-    mysql_query("UPDATE user SET updated_at = '$current_date' WHERE fb_id = $fb_id");
-    $array_response["status"] = "1";
-    $array_response["action"] = "record updated";
-}
-echo json_encode($array_response);
+echo json_encode($response);
 ?>
