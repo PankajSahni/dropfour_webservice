08829278e7c04e41e04a7ba5f6796f2f43fd81f3
Akhlaq Ahmad <amu.akhlaq@gmail.com>
2012-10-11 11:40:50 +0530

    added functionality to get all invites from server

--- all_data.php
+++ all_data.php
@@ -1,16 +1,26 @@
 <?php
 include("includes/config.php");
 
-$fb_id = $_REQUEST['fb_id'];
+$json_data = json_decode(stripslashes($_REQUEST['json_data']), true);
 
 $response = array();
 
-$game_invite_res = mysql_query("select * from game g inner join user u on g.player_two=u.id where u.fb_id = '".$fb_id."' and g.status=0") or die(mysql_error());
-$game_invites = array();
-while($game_invite_line = mysql_fetch_assoc($game_invite_res)){
-	$game_invites[] = array($game_invite_line['fb_id'], $game_invite_line['name']);
+if(isset($json_data) && count($json_data) && $json_data['fb_id']!=''){
+	$fb_id = $json_data['fb_id'];
+	$game_invite_sql = "select * from game g inner join user u on g.player_one=u.id where g.player_two in (select id from user u where u.fb_id = '".$fb_id."' and g.status=0)";
+	$game_invite_res = mysql_query($game_invite_sql) or die(mysql_error());
+	$game_invites = array();
+	while($game_invite_line = mysql_fetch_assoc($game_invite_res)){
+		$game_invites[] = array($game_invite_line['fb_id'], $game_invite_line['name']);
+	}
+	$response['game_invites'] = $game_invites;
+
+}else{
+	$response['STATUS'] = "0";
+	$response['MESSAGE'] = "Facebook Id is not provided!";
 }
-$response['game_invites'] = $game_invites;
+
+
 
 echo json_encode($response);
 ?>
