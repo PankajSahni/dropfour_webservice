75bd924cd90d54e60a91439661f7e924be7b46a0
Akhlaq Ahmad <amu.akhlaq@gmail.com>
2012-10-11 11:56:13 +0530

    added functionality to get all invites from server

--- all_data.php
+++ all_data.php
@@ -7,11 +7,11 @@ $response = array();
 
 if(isset($json_data) && count($json_data) && $json_data['fb_id']!=''){
 	$fb_id = $json_data['fb_id'];
-	$game_invite_sql = "select * from game g inner join user u on g.player_one=u.id where g.player_two in (select id from user u where u.fb_id = '".$fb_id."' and g.status=0)";
+	$game_invite_sql = "select g.id game_id, * from game g inner join user u on g.player_one=u.id where g.player_two in (select id from user u where u.fb_id = '".$fb_id."' and g.status=0)";
 	$game_invite_res = mysql_query($game_invite_sql) or die(mysql_error());
 	$game_invites = array();
 	while($game_invite_line = mysql_fetch_assoc($game_invite_res)){
-		$game_invites[] = array($game_invite_line['fb_id'], $game_invite_line['name']);
+		$game_invites[] = array('game_id' => $game_invite_line['game_id'], id => $game_invite_line['fb_id'], 'name' => $game_invite_line['name']);
 	}
 	$response['game_invites'] = $game_invites;
 
