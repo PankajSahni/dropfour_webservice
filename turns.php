a9a4172c59dcb849bb26d81deb530a872bea3526
Akhlaq Ahmad <amu.akhlaq@gmail.com>
2012-12-11 16:43:53 +0530

    added update turn functionality

--- turns.php
+++ turns.php
@@ -11,24 +11,36 @@ if (mysql_num_rows($query_get_last_move_user) == 1) {
     $last_move_by_user = $query_get_last_move_user_row['move'];
     if ($last_move_by_user != $json_array['turn_by']) {
         $current_date = date("Y-m-d H:i:s");
-        $sql_insert_turn = "INSERT INTO turns SET
-                                turn_by = '" . $json_array['turn_by'] . "',
-                                file_name = '" . $json_array['file_name'] . "',
-                                game_id = '" . $json_array['game_id'] . "',
-                                status = '" . $json_array['status'] . "',
-                                position_x = '" . $json_array['position_x'] . "',
-                                position_y = '" . $json_array['position_y'] . "',
-                                moving = '" . $json_array['moving'] . "',
-				                created_at='$current_date',
-				                updated_at='$current_date'";
-        $sql_update_game = "UPDATE game SET
-                                move = '" . $json_array['turn_by'] . "',
-				updated_at='$current_date'
-                                WHERE id = '" . $json_array['game_id'] . "'";
-        mysql_query($sql_update_game) or die(mysql_error());
-        mysql_query($sql_insert_turn) or die(mysql_error());
-        $response['STATUS'] = "1";
-        $response['MESSAGE'] = "Turn saved successfully";
+        if($json_array['action']=='ADD'){
+            $sql_insert_turn = "INSERT INTO turns SET
+                                    turn_by = '" . $json_array['turn_by'] . "',
+                                    file_name = '" . $json_array['file_name'] . "',
+                                    game_id = '" . $json_array['game_id'] . "',
+                                    status = '" . $json_array['status'] . "',
+                                    position_x = '" . $json_array['position_x'] . "',
+                                    position_y = '" . $json_array['position_y'] . "',
+                                    moving = '" . $json_array['moving'] . "',
+    				                created_at='$current_date',
+    				                updated_at='$current_date'";
+            $sql_update_game = "UPDATE game SET
+                                    move = '" . $json_array['turn_by'] . "',
+    				updated_at='$current_date'
+                                    WHERE id = '" . $json_array['game_id'] . "'";
+            mysql_query($sql_update_game) or die(mysql_error());
+            mysql_query($sql_insert_turn) or die(mysql_error());
+            $response['STATUS'] = "1";
+            $response['MESSAGE'] = "Turn saved successfully";
+        }elseif($json_array['action']=='UPDATE'){
+            $sql_update = "update tirns set moving = '".$json_array['moving']."',
+                                status = '" . $json_array['status'] . "' where game_id='".$json_array['game_id']."' and position_x = '" . $json_array['position_x'] . " and position_y = '" . $json_array['position_y'];
+            mysql_query($sql_update) or die(mysql_error());
+            $response['STATUS'] = "1";
+            $response['MESSAGE'] = "Turn saved successfully";
+        }else{
+            $response['STATUS'] = "0";
+            $response['MESSAGE'] = "Request not containing correct action";
+        }
+        
     } else {
         $response['STATUS'] = "0";
         $response['MESSAGE'] = "You Already Played Your turn";
