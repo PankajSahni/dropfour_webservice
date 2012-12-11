1481e09f7d66edcd6a63536ba71766a3f6e2ab41
Akhlaq Ahmad <amu.akhlaq@gmail.com>
2012-12-11 18:50:04 +0530

    added functionality to get last turn

--- get_all_turns_for_game.php
+++ get_all_turns_for_game.php
@@ -6,7 +6,7 @@ $array_turn_rows = array();
 $json_data = stripslashes($_REQUEST['json_data']);
 $json_array = json_decode($json_data, TRUE);
 $game_id = $json_array['game_id'];
-$query_get_all_turns_for_game = mysql_query("SELECT * FROM turns WHERE game_id = '$game_id'");
+$query_get_all_turns_for_game = mysql_query("SELECT * FROM turns WHERE game_id = '$game_id' order by id desc ".($json_array['action']=='LASTTURN'?' Limit 0, 1':''));
 if (mysql_num_rows($query_get_all_turns_for_game) > 0) {
     while ($query_get_all_turns_for_game_row = mysql_fetch_array($query_get_all_turns_for_game)) {
         $array_turn_rows[] = $query_get_all_turns_for_game_row;
